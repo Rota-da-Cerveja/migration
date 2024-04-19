190458526fc29213ac924dc8d94a8536bcab1f21
HerberthSantos7 <herberthfelipe1234@gmail.com>
2024-04-19 09:54:13 -0300

    Adicionando os scripts da tabela Inputs e SideDishes

---
+++ src/V1.5__Create_Table_Inputs.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `mydb`.`inputs` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(100) NOT NULL,
+  `isAllergic` TINYINT NOT NULL,
+  `img` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB
